bacdd2810a4a8bae926eff944eeb72310dadddb1
André R <andre.romcke@gmail.com>
2016-07-12 09:24:46 +0200

    Revert "EZP-25771: Inject block into custom-fetchers."

--- packages/ezflow_extension/ezextension/ezflow/classes/ezflowoperations.php
+++ packages/ezflow_extension/ezextension/ezflow/classes/ezflowoperations.php
@@ -73,7 +73,6 @@ class eZFlowOperations
         $fetchClassOptions->iniFile = 'block.ini';
         $fetchClassOptions->iniSection = $block['block_type'];
         $fetchClassOptions->iniVariable = 'FetchClass';
-        $fetchClassOptions->handlerParams = array( new eZFlowFetchParameters( $block ) );
 
         $fetchInstance = eZExtension::getHandlerClass( $fetchClassOptions );
 
